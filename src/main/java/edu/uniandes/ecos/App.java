3dc9877b59ba5eebc270bfab428c74252288e174
John Dany Osorio Ramos <johnoso27@hotmail.com>
2015-01-24 10:16:08 -0500

    Segundo envio

---
+++ src/main/java/edu/uniandes/ecos/App.java
@@ -0,0 +1,13 @@
+package edu.uniandes.ecos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
